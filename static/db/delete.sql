3fb89cdf7afb832751225cd28cea64eea375699a
Wolihts <167509032+Wolihts@users.noreply.github.com>
2025-08-14 16:49:19 -0400

    delete.sql feedback

--- static/db/delete.sql
+++ static/db/delete.sql
@@ -1,13 +1,15 @@
-DROP TABLE IF EXISTS habits.weight;
-
-DROP TABLE IF EXISTS habits.workout;
-
-DROP TABLE IF EXISTS habits.diet;
+DROP TABLE IF EXISTS habits.feedback;
 
 DROP TABLE IF EXISTS habits.sleep;
 
-DROP TABLE IF EXISTS habits.user_detail;
+DROP TABLE IF EXISTS habits.diet;
+
+DROP TABLE IF EXISTS habits.workout;
+
+DROP TABLE IF EXISTS habits.weight;
 
 DROP TABLE IF EXISTS habits.users;
 
-DROP SCHEMA habits;
+DROP TABLE IF EXISTS habits.user_detail;
+
+DROP SCHEMA IF EXISTS habits;
